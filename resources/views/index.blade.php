2017e2b220e8abfe24dd692b0f46bd4c63ea3464
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-01-04 20:59:13 +0700

    add: form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,18 @@
 <h1 class="mt-3">Absen</h1>
 
 <form method="POST" action="/">
-  
-  <button type="submit" class="btn btn-primary">Create Course</button>
+  <div class="form-group">
+      <input id="nim" autocomplete="off" autofocus class="form-control" name="nim" placeholder="NIM" type="text">
+  </div>
+  <div class="form-group">
+      <input id="password" class="form-control" name="password" placeholder="Password" type="password">
+  </div>
+  <select id="matkul" name="matkul" class="form-control">
+      <option value="sim" selected>SIM</option>
+  </select>
+  <select id="fakultas" name="fakultas" class="form-control">
+      <option value="fst" selected>FST</option>
+  </select>
+  <button type="submit" class="btn btn-primary">Konfirmasi</button>
 </form>
 @endsection
