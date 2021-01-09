209e10e5cfee8c3b3d8d3937da1770d8dba7f4c4
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-01-09 16:30:15 +0700

    add: Model

--- app/Models/Absence.php
+++ app/Models/Absence.php
@@ -8,5 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Absence extends Model
 {
     use HasFactory;
-    public $timestamps = false;
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_nim', 'nim');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
