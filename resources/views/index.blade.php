e277bb92d73c601958968e36f2516a3354e249d9
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2021-01-11 21:36:56 +0700

    fix: fix options in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,28 +3,24 @@
 @section('title', 'Presensi')
 
 @section('container')
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
-    </div>
-@endif
 <h1 class="mt-3">Absen</h1>
 
 <form method="POST" action="/">
-  @csrf
-  <div class="form-group">
-    <input id="nim" autocomplete="off" autofocus class="form-control" name="nim" placeholder="NIM" type="text">
-  </div>
-  <div class="form-group">
-    <input class="form-control" name="password" placeholder="Password" type="password">
-  </div>
-  <select name="matkul" class="form-control">
-    <option value="1">SIM</option>
-    <option value="2">Manrisk</option>
-  </select>
-  <select name="fakultas" class="form-control">
-    <option value="fst" selected>FST</option>
-  </select>
-  <button type="submit" class="btn btn-primary">Konfirmasi</button>
+    @csrf
+    <div class="form-group">
+        <input id="nim" autocomplete="off" autofocus class="form-control" name="nim" placeholder="NIM" type="text">
+    </div>
+    <div class="form-group">
+        <input class="form-control" name="password" placeholder="Password" type="password">
+    </div>
+    <select name="matkul" class="form-control">
+        @foreach($courses as $course)
+        <option value="{{$course->id}}">{{$course->matkul}}</option>
+        @endforeach
+    </select>
+    <select name="fakultas" class="form-control">
+        <option value="fst" selected>FST</option>
+    </select>
+    <button type="submit" class="btn btn-primary">Konfirmasi</button>
 </form>
 @endsection
